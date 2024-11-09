a82e8e690ce1be648fb9fb666c12e1b66ccdfbe3
duchnuon <duchnuon@gmail.com>
2024-11-09 18:17:04 +0700

    completed register service providers

---
+++ src/Providers/PdfImgKhServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace KhmerPdfImg\LaravelKhPdfImg\Providers;
+use Illuminate\Support\ServiceProvider;
+
+class PdfImgKhServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+    }
+
+    public function boot()
+    {
+
+    }
+}
